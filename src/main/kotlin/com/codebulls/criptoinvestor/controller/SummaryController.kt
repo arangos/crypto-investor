349190425db3c0bdba8e472f8485596388affc53
andres.arangom <andres.arango@aptitude.com>
2025-07-22 22:51:45 -0500

    Fixing package name to follow conventions

--- src/main/kotlin/com/codebulls/criptoinvestor/controller/SummaryController.kt
+++ src/main/kotlin/com/codebulls/criptoinvestor/controller/SummaryController.kt
@@ -1,10 +1,9 @@
-package com.codebulls.criptoinvestor.Controller
-
+package com.codebulls.criptoinvestor.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import com.codebulls.criptoinvestor.Service.SummaryService
+import com.codebulls.criptoinvestor.service.SummaryService
 
 @RestController
 @RequestMapping("/summary")
